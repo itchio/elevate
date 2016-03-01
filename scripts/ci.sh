0ec0a7c9cacd9f24b0582e040c9ee672cc3bd168
Amos Wenger <fasterthanlime@gmail.com>
2016-03-01 16:56:43 +0100

    Add gitlab ci config

---
+++ scripts/ci.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -xe
+
+7za | head -2
+gcc -v
+cppcheck --error-exitcode=1 src
+
+export ELEVATE_VERSION=head
+if [ -n "$CI_BUILD_TAG" ]; then
+  export ELEVATE_VERSIOn=$CI_BUILD_TAG
+fi
+export ELEVATE_CFLAGS="-DELEVATE_VERSION=\\\"$ELEVATE_VERSION\\\""
+
+
+if [ "$CI_ARCH" = "amd64" ]; then
+  export PATH=/mingw64/bin:$PATH
+else
+  export PATH=/mingw32/bin:$PATH
+fi
+
+make
+file elevate.exe
+
+if [ -n "$CI_BUILD_TAG" ]; then
+  7za a elevate.7z elevate.exe
+  echo $CI_BUILD_TAG > LATEST
+fi
+

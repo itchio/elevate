ca2c51939fa5f723dd5c675ddd3588f84a1c280c
Amos Wenger <fasterthanlime@gmail.com>
2016-06-13 11:10:39 +0200

    gsutil -m

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # upload all artifacts from a single worker
-gsutil cp -r -a public-read binaries/* gs://dl.itch.ovh/elevate/
+gsutil -m cp -r -a public-read binaries/* gs://dl.itch.ovh/elevate/
